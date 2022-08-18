59eb9bd33c5f9aa34980b3fd7271825bba3532f5
Mau <mauriciom.curiel@gmail.com>
2022-08-17 20:40:32 -0700

    updates javascript to work

--- home.js
+++ home.js
@@ -1,3 +1,7 @@
-document.querySelector('.flipper').addEventListener('click', event => {
-    this.querySelector('.flipper-card').toggleClass('flipper-is-flipped');
-})
+//adds on click functionality to flip project cards.
+document.querySelectorAll('.flipper-card')
+    .forEach(function(card){
+        card.addEventListener('click', function (){
+            this.classList.toggle('flipper-is-flipped');
+        })
+    });
