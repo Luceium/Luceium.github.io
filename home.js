dfd38009d0347007b03da7fd9498c835b30152c9
Mau <mauriciom.curiel@gmail.com>
2022-09-23 20:37:57 -0700

    adds project

--- home.js
+++ home.js
@@ -1,3 +1,14 @@
+//home page picture change test
+let backgrounds = ["astronomicalMorning", "batGradient", "mexBeach", "rainyRocks", "rollingWaves", "schoolPink", "sf", "suspension", "windmill"];
+let i = 0;
+
+document.querySelector('header').addEventListener('click', function(){
+        i = (i + 1) % backgrounds.length;
+        console.log(`url('images/homepagePic/${backgrounds[i]}.JPG')`);
+        this.style.backgroundImage = `url('images/homepagePic/${backgrounds[i]}.JPG')`;
+    })
+//
+
 //adds on click functionality to flip project cards.
 document.querySelectorAll('.flipper-card')
     .forEach(function(card){
@@ -5,3 +16,4 @@ document.querySelectorAll('.flipper-card')
             this.classList.toggle('flipper-is-flipped');
         })
     });
+//
